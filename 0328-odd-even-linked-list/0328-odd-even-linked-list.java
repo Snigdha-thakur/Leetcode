8b2a4758cd1014c9fc80795d8b56cbcf8f5b085c
Snigdha-thakur <148651035+Snigdha-thakur@users.noreply.github.com>
2025-07-22 07:40:19 +0530

    Time: 0 ms (100%), Space: 44.7 MB (49.28%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        //check if our list is empty or not
+        if(head==null || head.next==null) return head;
+        ListNode odd= head;
+        ListNode even=head.next;
+        ListNode evenHead=even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next; //taking next of even as next of head node so we can skip the even part and join odd first
+            odd = odd.next; //this will keep moving our odd forward
+            even.next = odd.next; //connect even with next even
+            even = even.next; //move even forward
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
