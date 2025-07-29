0f7450b520f4dd0eb1ded3afabc3506ba1f7b27a
Snigdha-thakur <148651035+Snigdha-thakur@users.noreply.github.com>
2025-07-29 11:38:48 +0530

    Time: 0 ms (100%), Space: 41.1 MB (5.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,4 @@
-//1. Recursive Approach (Exponential Time)
+// 1. Recursive Approach (Exponential Time)
 // class Solution {
 //     public int climbStairs(int n) {
 //         if (n <= 1) return 1;
@@ -7,34 +7,34 @@
 // }
 
 
-//2. DP Top-Down (Recursion + Memoization)
-// class Solution {
-//     public int climbStairs(int n) {
-//         int[] dp = new int[n + 1];
-//         Arrays.fill(dp, -1);
-//         return helper(n, dp);
-//     }
-
-//     private int helper(int n, int[] dp) {
-//         if (n <= 1) return 1;
-//         if (dp[n] != -1) return dp[n];
-//         return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-//     }
-// }
-
-
-//Iterative Bottom-Up DP (Tabulation)
+ //2. DP Top-Down (Recursion + Memoization)
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 1) return 1;
         int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        
-        return dp[n];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
+    }
+
+    private int helper(int n, int[] dp) {
+        if (n <= 1) return 1;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     }
 }
+
+
+// //3. Iterative Bottom-Up DP (Tabulation)
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n <= 1) return 1;
+//         int[] dp = new int[n + 1];
+//         dp[0] = 1;
+//         dp[1] = 1;
+        
+//         for (int i = 2; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+        
+//         return dp[n];
+//     }
+// }
